b5b3e3f9f2f26c41e8830f87a44cd41d213587c7
Daniel Cazzulino <daniel@cazzulino.com>
2015-01-05 18:06:36 -0300

    Initial build and test for roslyn-based moq discovery

---
+++ src/Moq.Build.Tests/RoslynTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.MSBuild;
+using Xunit;
+
+namespace Moq.Build.Tests
+{
+	public class RoslynTests
+	{
+		[Fact]
+		public async Task when_loading_project_then_succeeds ()
+		{
+			var workspace = MSBuildWorkspace.Create ();
+			var project = await workspace.OpenProjectAsync (@"..\..\Moq.Build.Tests.csproj");
+
+			Console.WriteLine (project.GetCompilationAsync ().Result.AssemblyName);
+		}
+	}
+}
---
+++ src/Moq.Build/GenerateProxies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Moq.Build
+{
+	public class GenerateProxies : Task
+	{
+		[Required]
+		public ITaskItem SourceProject { get; set; }
+
+		[Required]
+		public string TargetFile { get; set; }
+
+		public override bool Execute ()
+		{
+			return false;
+		}
+	}
+}
